02c41b93ccbcc2389415ef9d1e2a6325a4088a27
Tenges <152248859+Signet85@users.noreply.github.com>
2025-07-23 14:32:30 +0800

    3.20 register form create

---
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -0,0 +1,68 @@
+
+'use client'
+import { registerSchema, RegisterSchema } from '@/lib/schemas/registerSchema';
+import { Card, CardHeader, CardBody, Input, Button } from '@heroui/react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { GiPadlock } from 'react-icons/gi';
+
+export default function LoginForm() {
+    const {register, handleSubmit, formState: {errors, isValid}} = useForm<RegisterSchema>({
+        resolver: zodResolver(registerSchema),
+        mode:'onTouched',
+    });
+
+    const onSubmit = (data: RegisterSchema) => {
+        console.log(data)
+    }
+  return (
+    <Card className='w-2/5 mx-auto'>
+        <CardHeader className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col items-center text-secondary'>
+                <div className='flex flex-row items-center gap-3'>
+                <GiPadlock size={30}/>
+                <h1 className='text-3xl font-semibold'>Register</h1>
+                </div>
+                <p className='text-neutral-500'>Welcome to NextMatch</p>
+            </div>
+        </CardHeader>
+        <CardBody>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='spacy-y-4'>
+                      <Input
+                        defaultValue=''
+                        label='Name'
+                        variant='bordered'
+                        {...register('name')}
+                        isInvalid = {!!errors.name}
+                        errorMessage = {errors.name?.message}
+                    />
+                    <Input
+                        defaultValue=''
+                        label='Email'
+                        variant='bordered'
+                        {...register('email')}
+                        isInvalid = {!!errors.email}
+                        errorMessage = {errors.email?.message}
+                    />
+                    <Input
+                        
+                        defaultValue=''
+                        label='Password'
+                        variant='bordered'
+                        type='password'
+                        {...register('password')}
+                        isInvalid = {!!errors.password}
+                        errorMessage = {errors.password?.message}
+
+                    />
+                    <Button isDisabled={!isValid} fullWidth color='secondary' type='submit'>
+                        Register
+                    </Button>
+                </div>
+            </form>
+        </CardBody>
+    </Card>
+  )
+}
